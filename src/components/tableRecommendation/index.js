ecff982ad84662fcea5fba799134c4e905d709b6
Daffa <daffamaulana30@gmail.com>
2020-12-21 23:37:10 +0800

    ready

--- src/components/tableRecommendation/index.js
+++ src/components/tableRecommendation/index.js
@@ -20,7 +20,9 @@ const TableRecommendation = ({ data }) => {
           <th>Genre</th>
         </tr>
       </thead>
-      <tbody style={{ color: 'white' }}>{listMusic}</tbody>
+      <tbody style={{ color: 'white', fontWeight: 'lighter' }}>
+        {listMusic}
+      </tbody>
     </Table>
   );
 };
