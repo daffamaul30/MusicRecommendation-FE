d45e4a828abf9a227493887a2ab5e4174aae5f89
Daffa <daffamaulana30@gmail.com>
2020-12-21 22:03:31 +0800

    ready

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { getCookie } from '../utils/cookie';
+// import { getCookie } from '../utils/cookie';
 
-function getTokenAuth() {
-  if (getCookie('token') && getCookie('userData')) {
-    return JSON.parse(getCookie('token')).value;
-  }
-  return '';
-}
+// function getTokenAuth() {
+//   if (getCookie('token') && getCookie('userData')) {
+//     return JSON.parse(getCookie('token')).value;
+//   }
+//   return '';
+// }
 
 const createAxiosInterceptor = (url) => {
   const axiosCreate = axios.create({
@@ -15,7 +15,7 @@ const createAxiosInterceptor = (url) => {
       Accept: 'application/json',
       'Accept-Language': 'es',
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${getTokenAuth()}`,
+      // Authorization: `Bearer ${getTokenAuth()}`,
     },
   });
   axiosCreate.interceptors.response.use(
